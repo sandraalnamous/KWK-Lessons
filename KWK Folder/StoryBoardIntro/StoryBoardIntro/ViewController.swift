bef79095a56be5b50050e99ced749e6a48e5f512
Sandra Alnamous <76766645+sandraalnamous@users.noreply.github.com>
2021-07-28 11:39:49 -0400

    1st Commit

---
+++ KWK Folder/StoryBoardIntro/StoryBoardIntro/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StoryBoardIntro
+//
+//  Created by Scholar on 7/28/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("this is a print statement - testing testing!")
+    }
+
+
+}
+
